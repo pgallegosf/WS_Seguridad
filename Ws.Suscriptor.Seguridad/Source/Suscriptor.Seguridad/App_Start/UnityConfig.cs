8f25a30352156dc42fbd0edd4b97fa1d1c784ec1
Olga <kathy.16k@hotmail.com>
2020-01-25 01:07:36 -0500

    Modificacion de referencias

--- Ws.Suscriptor.Seguridad/Source/Suscriptor.Seguridad/App_Start/UnityConfig.cs
+++ Ws.Suscriptor.Seguridad/Source/Suscriptor.Seguridad/App_Start/UnityConfig.cs
@@ -1,6 +1,4 @@
 using System.Web.Http;
-using Domain.Services;
-using Domain.Services.Contracts;
 using Unity;
 using Unity.WebApi;
 
@@ -11,12 +9,12 @@ namespace Lp.Suscriptor.Seguridad
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            container.RegisterType<IActiveDirectory, NActiveDirectory>();      
-            container.RegisterType<IToken, NToken>();
-            container.RegisterType<IUser, NUser>();
-            container.RegisterType<IRol, NRol>();
-            container.RegisterType<ILog, NLog>();
-            container.RegisterType<IAccesoError, NAccesoError>();
+            
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+            
+            // e.g. container.RegisterType<ITestService, TestService>();
+            
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
