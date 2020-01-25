70a4a956b10ccb0dd62ee9f625b534af4ca9fc8a
Olga <kathy.16k@hotmail.com>
2020-01-25 17:25:23 -0500

    resolviendo conflictos de actualización

--- Ws.Suscriptor.Seguridad/Source/Suscriptor.Seguridad/App_Start/UnityConfig.cs
+++ Ws.Suscriptor.Seguridad/Source/Suscriptor.Seguridad/App_Start/UnityConfig.cs
@@ -1,4 +1,6 @@
 using System.Web.Http;
+using Domain.Services;
+using Domain.Services.Contracts;
 using Unity;
 using Unity.WebApi;
 
@@ -8,13 +10,13 @@ namespace Lp.Suscriptor.Seguridad
     {
         public static void RegisterComponents()
         {
-			var container = new UnityContainer();
-            
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
-            
-            // e.g. container.RegisterType<ITestService, TestService>();
-            
+            var container = new UnityContainer();
+            container.RegisterType<IActiveDirectory, NActiveDirectory>();
+            container.RegisterType<IToken, NToken>();
+            container.RegisterType<IUser, NUser>();
+            container.RegisterType<IRol, NRol>();
+            container.RegisterType<ILog, NLog>();
+            container.RegisterType<IAccesoError, NAccesoError>();
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
